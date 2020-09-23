9040b2c6848bb213aa3ba22462d298e4126774fa
daniloday <danil.vladyka1@gmail.com>
2020-09-23 10:51:07 +0300

    fix

--- custom_schedule.py
+++ custom_schedule.py
@@ -9,7 +9,7 @@ class Lesson():
 def create_schedule():
     schedule = (
         Lesson( name = 'СРОМ 2 (практика) | Черный Олег', week = 0, day = '1', time = "08:20", zoom = "https://us04web.zoom.us/j/2625120734?pwd=K0QrcXN4ekcrbDZKRW15Z1NVUSs3UT09"), 
-        Lesson( name = 'Теор.вер (лекция) | Дор А.А', week = 3, day = '1', time = "10:15", zoom = "Пару я дал, а ссылку я не дам"), 
+        Lesson( name = 'Теор.вер (лекция) | Дор А.А', week = 3, day = '1', time = "10:15", zoom = "https://us02web.zoom.us/j/82324302808"), 
         Lesson( name = 'Мат.мод (лекция) | Смирнов С.А', week = 3, day = '1', time = "12:10", zoom = "https://meet.google.com/lookup/dypflshjwm?authuser=0&hs=179"), 
         Lesson( name = 'English | Синекоп', week = 3, day = '1', time = "14:05", zoom = "https://us04web.zoom.us/j/9822532040?pwd=c21vVFBxbDcyNldYYmxBK3c0ckZXdz09"),
 
@@ -19,7 +19,7 @@ def create_schedule():
         Lesson( name = 'Теория сложности (практика) | Фес Андрей', week = 0, day = '3', time = "08:20", zoom = "https://us04web.zoom.us/j/3033669459?pwd=N0ovVUV3dTQ4N2xmaDBpTTdCQ3BBUT09"), 
         Lesson( name = 'Методы оптимизаций (практика) | Дед Данилов В.Я', week = 3, day = '3', time = "10:15", zoom = "https://us04web.zoom.us/j/77043249383?pwd=ZkN5amE3RFFPT3IyalZENTQ3QVl2dz09"), 
         Lesson( name = 'QAQC (лекция) | Ткач В.М', week = 1, day = '3', time = "12:10", zoom = "https://bth.zoom.us/j/62733592448"), 
-        Lesson( name = 'Мат.прога | Хмельницкий ', week = 3, day = '3', time = "14:05", zoom = "В группе мат.прога (там даже ссылки на группу нет)"),
+        Lesson( name = 'Мат.прога | Хмельницкий ', week = 3, day = '3', time = "14:05", zoom = "Пару я дал, а ссылку я не дам \n В группе мат.прога (там даже ссылки на группу нет)"),
 
         Lesson( name = 'Теория сложности (лекция) | Фес Андрей', week = 3, day = '4', time = "08:20", zoom = "https://us04web.zoom.us/j/3033669459?pwd=N0ovVUV3dTQ4N2xmaDBpTTdCQ3BBUT09"), 
         Lesson( name = 'Теор.вер (практика) | Нищенко И.И', week = 3, day = '4', time = "10:15", zoom = "https://us02web.zoom.us/j/82678565122"), 
