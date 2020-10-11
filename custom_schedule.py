5c1a2347417c8ee37f5fe73db253adae660747f5
daniloday <danil.vladyka1@gmail.com>
2020-10-12 01:43:52 +0300

    fix links

--- custom_schedule.py
+++ custom_schedule.py
@@ -9,8 +9,8 @@ class Lesson():
 def create_schedule():
     schedule = (
         Lesson( name = 'СРОМ 2 (практика) | Черный Олег', week = 0, day = '1', time = "8:20", zoom = "https://us04web.zoom.us/j/2625120734?pwd=K0QrcXN4ekcrbDZKRW15Z1NVUSs3UT09"), 
-        Lesson( name = 'Теор.вер (лекция) | Дор А.А', week = 3, day = '1', time = "10:15", zoom = "https://us02web.zoom.us/j/82324302808"), 
-        Lesson( name = 'Мат.мод (лекция) | Смирнов С.А', week = 3, day = '1', time = "12:10", zoom = "https://meet.google.com/lookup/dypflshjwm?authuser=0&hs=179"), 
+        Lesson( name = 'Теор.вер (лекция) | Дор А.А', week = 3, day = '1', time = "10:15", zoom = "https://us02web.zoom.us/j/82324302808?pwd=M2x0emhVdldBV2F4MnhqWjRCWHBRQT09"), 
+        Lesson( name = 'Мат.мод (лекция) | Смирнов С.А', week = 3, day = '1', time = "12:10", zoom = "https://meet.google.com/lookup/asny3chck7?authuser=1&hs=179"), 
         Lesson( name = 'English | Синекоп', week = 3, day = '1', time = "14:05", zoom = "https://us04web.zoom.us/j/9822532040?pwd=c21vVFBxbDcyNldYYmxBK3c0ckZXdz09"),
 
         Lesson( name = 'Методы оптимизаций (лекция) | Дед Данилов В.Я', week = 3, day = '2', time = "10:15", zoom = "https://us04web.zoom.us/j/73389991708?pwd=aDZ2YmNqRG9rb2dQdW4zcWdERDhVZz09"), 
@@ -22,7 +22,7 @@ def create_schedule():
         Lesson( name = 'Мат.прога | Хмельницкий', week = 3, day = '3', time = "14:05", zoom = "Пару я дал, а ссылку я не дам \n В группе мат.прога (там даже ссылки на группу нет)"),
 
         Lesson( name = 'Теория сложности (лекция) | Фес Андрей', week = 3, day = '4', time = "8:20", zoom = "https://us04web.zoom.us/j/3033669459?pwd=N0ovVUV3dTQ4N2xmaDBpTTdCQ3BBUT09"), 
-        Lesson( name = 'Теор.вер (практика) | Нищенко И.И', week = 3, day = '4', time = "10:15", zoom = "https://us02web.zoom.us/j/82678565122"), 
+        Lesson( name = 'Теор.вер (практика) | Нищенко И.И', week = 3, day = '4', time = "10:15", zoom = "https://meet.google.com/vpz-fcaf-wtt"), 
         Lesson( name = 'СРОМ 1 (лекция) | Завадская Л.О.', week = 3, day = '4', time = "12:10", zoom = "https://us04web.zoom.us/j/3908947683?pwd=UWhmb1B0NDZSU3dZc3ZVL0RaSUJqdz09"), 
 
         Lesson( name = 'СРОМ 1 (лабы) | Пекарчук Нина', week = 0, day = '5', time = "8:20", zoom = "https://us04web.zoom.us/j/8397990765?pwd=dUMzWk1hSWx2RWtrTTRHalNObXhWUT09 но это не точно"), 
