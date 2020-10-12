11eba38709f34064f97b52dcf67582d2f34ef027
daniloday <danil.vladyka1@gmail.com>
2020-10-12 12:23:43 +0300

    fix

--- bot.py
+++ bot.py
@@ -3,43 +3,6 @@ import custom_schedule
 import time
 import telepot
 
-class TelegramBot():
-
-    def __init__(self, url):
-        self.url = url
-        # self.chat_id = -1001187367399
-        # chat_id = -1001367674629
-
-    def get_updates(self):
-        response = requests.get(self.url + 'getUpdates')
-        return response.json()
-
-    def last_update(self):
-        results = self.get_updates()['result']
-        last_updates = len(results) - 1
-        return results[last_updates]
-
-    def get_last_chat_id(self):
-        return self.last_update()['message']['chat']['id']
-
-    def send_message(self, text, chat_id):
-        params = {'chat_id' : chat_id, 'text' : text}
-        response = requests.post(self.url + 'sendMessage', data=params)
-        return response
-
-    def pin_message(self, message_id, chat_id):
-        params = {'chat_id' : chat_id, 'message_id' : message_id, "disable_notification" : False}
-        response = requests.post(self.url + 'pinChatMessage', data=params)
-        return response
-
-    # def send_photo(self, path, chat_id):
-    #     image = Image.open(path)
-    #     image.get
-    #     photo = open(path, 'rb')
-    #     photo.
-    #     params = {'chat_id' : chat_id, 'photo' : photo}
-    #     response = requests.post(self.url + 'sendMessage', data=params)
-    #     return response
 
 
 
@@ -58,13 +21,13 @@ def main():
             if lesson.day == time.strftime("%w") and lesson.time == (str(int(time.strftime("%H"))+3)+time.strftime(":%M")):
                 if lesson.week == 3 or lesson.week == int(time.strftime("%W")) % 2:
                     chat_id = -1001187367399
-                    resp = bot.sendMessage(chat_id, lesson.name + '\n' + lesson.zoom)
-                    time.sleep(1)
-                    bot.pinChatMessage(message_id = int(resp.json()["result"]["message_id"]), chat_id = chat_id)
+                    message_id = bot.sendMessage(chat_id, lesson.name + '\n' + lesson.zoom).message_id
+                    bot.pinChatMessage(chat_id = chat_id, message_id = message_id)
                     # if lesson.name == 'Мат.прога | Хмельницкий':
                         # bot.sendPhoto(chat_id, photo=open('1.jpg', 'rb'))
-                    if resp.status_code == 200:
-                        time.sleep(70)
+                    # if message_id != None:
+                    time.sleep(70)
+                        
 
 
 
